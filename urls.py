ba736cd66d1f9db996217282722d30b60f9c1555
Ben Best <ben.best@gmail.com>
2009-10-28 14:58:33 -0400

    adding home to urls - end of pomodoro commit.

--- urls.py
+++ urls.py
@@ -8,6 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 
+    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
     (r'^', include('pomodoro.urls')),
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
